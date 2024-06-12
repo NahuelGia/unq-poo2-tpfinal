9499436f93008963d513ca0980875edbcde6c7cd
Augusto Piana <augustopiana3@gmail.com>
2024-06-12 20:47:15 -0300

    arranco SEM y sus tests.

--- src/ar/edu/unq/poo2/tpfinal/SEM.java
+++ src/ar/edu/unq/poo2/tpfinal/SEM.java
@@ -1,40 +1,129 @@
 package ar.edu.unq.poo2.tpfinal;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SEM {
 
-	public void registrarEstacionamiento(EstacionadoPV estacionado) {
-		// TODO Auto-generated method stub
+	List<Estacionado> estacionados = new ArrayList<Estacionado>();
+	List<AppSEM> usuarios = new ArrayList<AppSEM>();
+	List<Ticket> tickets = new ArrayList<Ticket>();
+	List<ZonaEstacionamiento> zonasDeEstacionamientos = new ArrayList<ZonaEstacionamiento>();
+	List<Infraccion> infracciones = new ArrayList<Infraccion>();
+	private LocalTime inicioFranjaHoraria;
+	private LocalTime finFranjaHoraria;
+
+	public SEM() {
+		this.setInicioFranjaHoraria(LocalTime.of(7, 0));
+		this.setFinFranjaHoraria(LocalTime.of(20, 0));
 		
+	}
+
+	private void setFinFranjaHoraria(LocalTime of) {
+		this.finFranjaHoraria = LocalTime.of(20, 0);
+
+	}
+
+	private void setInicioFranjaHoraria(LocalTime of) {
+		this.inicioFranjaHoraria = LocalTime.of(7, 0);
+
+	}
+
+	public LocalTime getInicioFranjaHoraria() {
+		return inicioFranjaHoraria;
+
+	}
+
+	public LocalTime getfinFranjaHoraria() {
+		return finFranjaHoraria;
+	}
+
+
+	public List<Estacionado> getEstacionados() {
+		return estacionados;
+	}
+
+	public List<AppSEM> getUsuarios() {
+		return usuarios;
+	}
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public Double getPrecioPorHora() {
+		return 40.00;
+	}
+
+	public List<ZonaEstacionamiento> getZonasDeEstacionamiento() {
+		return zonasDeEstacionamientos;
+	}
+
+	public List<Infraccion> getInfracciones() {
+		return infracciones;
+	}
+
+	public void registrarEstacionamiento(Estacionado estacionado) {
+		this.getEstacionados().add(estacionado);
 	}
 
 	public void cargarSaldo(int telefono, double monto) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void registrarTicket( Ticket t) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void registrarTicket(Ticket t) {
+		this.tickets.add(t);
 
-	public Double getPrecioPorHora() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public void finalizarEstacionamiento(AppSEM appSEM) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void estacionamientoIniciado(AppSEM appSEM, String patente) {
-		// TODO Auto-generated method stub
 		
+		double horasPagables = (appSEM.getSaldo() / this.getPrecioPorHora());
+		LocalTime horaActual = LocalTime.now();
+		LocalTime horaMaxima = horaActual.plusHours((long) horasPagables);
+		
+		
+		int intHoraFin = Math.min(this.getfinFranjaHoraria().getHour(), horaMaxima.getHour());
+		
+		LocalTime horaFin = LocalTime.of(intHoraFin, 0);
+		
+		
+		EstacionadoAPP nuevoEstacionado = new EstacionadoAPP(patente, horaFin, appSEM.getNroTelefono());
+		
+		this.registrarEstacionamiento(nuevoEstacionado);
+		appSEM.notificarEstacionamientoExitoso(horaActual, horaFin);
+
+	}
+
+	public void liberarEstacionados() {
+		this.getEstacionados().removeAll(estacionados);
+	}
+
+	public void registrarUsuario(AppSEM appSEM) {
+		this.getUsuarios().add(appSEM);
+
+	}
+
+	public void agregarZonaEstacionamiento(String string) {
+
+		// creo nueva zona de estacionamiento.
+		ZonaEstacionamiento nuevaZona = new ZonaEstacionamiento(string);
+
+		// la agrego a la lista de zonas
+		this.getZonasDeEstacionamiento().add(nuevaZona);
+
 	}
 
 	public void registrarInfraccion(Infraccion infraccion) {
-		// TODO Auto-generated method stub
-		
+		this.getInfracciones().add(infraccion);
+
 	}
 
 	public boolean consultarVigencia(String patente) {
@@ -43,9 +132,4 @@ public class SEM {
 	}
 
 	
-
-	
-
-	
-
 }
--- test/ar/edu/unq/poo2/tpfinal/SEMTest.java
+++ test/ar/edu/unq/poo2/tpfinal/SEMTest.java
@@ -2,20 +2,151 @@ package ar.edu.unq.poo2.tpfinal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class SEMTest {
-	
+
+	SEM sem;
+	EstacionadoPV estacionadoPV;
+	EstacionadoPV estacionadoPV2;
+	TicketRecarga ticket;
+	AppSEM app;
+	ZonaEstacionamiento zona;
+
 	@BeforeEach
 	public void setUp() {
-		SEM sem = mock(SEM.class);
-		
+
+		sem = new SEM();
+		estacionadoPV = new EstacionadoPV("AAA-111", 6);
+		estacionadoPV2 = new EstacionadoPV("AAA-222", 6);
+		zona = new ZonaEstacionamiento("Quilmes");
+
 	}
-	
+
 	@Test
-	public void test1() {
-		
+	public void test1_CuandoUnSEMEsInicializadoSuListaDeEstacionadosEsVacia() {
+		assertTrue(sem.getEstacionados().isEmpty());
+	}
+
+	@Test
+	public void test2_UnSEMPuedeLiberarSuListaDeEstacionados() {
+
+		sem.registrarEstacionamiento(estacionadoPV);
+		sem.registrarEstacionamiento(estacionadoPV2);
+
+		sem.liberarEstacionados();
+
+		assertTrue(sem.getEstacionados().isEmpty());
+
+	}
+
+	@Test
+	public void test3_UnSEMPuedeAgregarUnEstacionamientoASuListaDeEstacionados() {
+
+		sem.registrarEstacionamiento(estacionadoPV);
+
+		assertFalse(sem.getEstacionados().isEmpty());
+
+	}
+
+	@Test
+	public void test4_CuandoUnSEMEsInicializadoSuListaDeUsuariosEsVacia() {
+		assertTrue(sem.getUsuarios().isEmpty());
+	}
+
+	@Test
+	public void test5_UnSEMPuedeRegistrarUnaAppASuListaDeUsuarios() {
+
+		sem.registrarUsuario(app);
+
+		assertFalse(sem.getUsuarios().isEmpty());
+
+	}
+
+	@Test
+	public void test6_CuandoUnSEMEsInicialziadoSuListaDeTicketsEsVacia() {
+		assertTrue(sem.getTickets().isEmpty());
+	}
+
+	@Test
+	public void test7_UnSEMPuedeAgregarUnTicketASuListaDeTickets() {
+
+		sem.registrarTicket(ticket);
+
+		assertFalse(sem.getTickets().isEmpty());
+
+	}
+
+	@Test
+	public void test8_CuandoUnSEMEsInicializadoSuListaDeZonasEsVacia() {
+		assertTrue(sem.getZonasDeEstacionamiento().isEmpty());
+	}
+
+	@Test
+	public void test9_UnSEMPuedeCrearYAgregarUnaZonaDeEstacionamientoASuListaDEZonas() {
+
+		sem.agregarZonaEstacionamiento("Quilmes");
+
+		assertFalse(sem.getZonasDeEstacionamiento().isEmpty());
+	}
+
+	@Test
+	public void test10_CuandoUnSEMEsInicializadoSuListaDeInfraccionesEsVacia() {
+		assertTrue(sem.getInfracciones().isEmpty());
+	}
+
+	@Test
+	public void test11_UnSEMPuedeRegistrarUnaInfraccionEnSuListaDeInfracciones() {
+
+		LocalDateTime fechaYHoraActual = LocalDateTime.now();
+		AppInspector appInspector = new AppInspector(sem, zona);
+
+		Infraccion infra = new Infraccion(appInspector, fechaYHoraActual, "AAA-111", zona);
+
+		sem.registrarInfraccion(infra);
+
+		assertFalse(sem.getInfracciones().isEmpty());
+
+	}
+
+	@Test
+	public void test12_ElSEMConoceSuInicioDeFranjaHoraria() {
+
+		LocalTime inicioFranjaHoraria = LocalTime.of(7, 0);
+
+		assertEquals(sem.getInicioFranjaHoraria(), inicioFranjaHoraria);
+
+	}
+
+	@Test
+	public void test12_ElSEMConoceSuFinDeFranjaHoraria() {
+
+		LocalTime finFranjaHoraria = LocalTime.of(20, 0);
+
+		assertEquals(sem.getfinFranjaHoraria(), finFranjaHoraria);
+
+	}
+
+	@Test
+	public void test13_ElSEMPuedeInicializarUnEstacionamientoEInteractuaConLaAPP() {
+
+		AppSEM mockApp = Mockito.mock(AppSEM.class);
+
+		sem.estacionamientoIniciado(mockApp, "AAA-111");
+
+		assertFalse(sem.getEstacionados().isEmpty());
+		verify(mockApp, times(1)).notificarEstacionamientoExitoso(any(LocalTime.class), any(LocalTime.class));
+
 	}
 
 }
