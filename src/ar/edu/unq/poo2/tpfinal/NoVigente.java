02ce17fa5e71b40e9cf0f7578c1159317486d724
Augusto Piana <augustopiana3@gmail.com>
2024-06-12 00:16:01 -0300

    termino NoVigente

--- src/ar/edu/unq/poo2/tpfinal/NoVigente.java
+++ src/ar/edu/unq/poo2/tpfinal/NoVigente.java
@@ -2,5 +2,23 @@ package ar.edu.unq.poo2.tpfinal;
 
 public class NoVigente extends EstadoEstacionamiento {
 
+	@Override
+	public void iniciar(AppSEM appSEM, String patente) {
+		if (appSEM.tieneSaldoMinimo()) {
+			appSEM.getSistema().estacionamientoIniciado(appSEM, patente);
+			appSEM.setEstado(new Vigente());
+		}
+		else {
+			appSEM.notificarSaldoInsuficiente();
+		}
+	}
+	
+	@Override
+	public void inicioNotificado(AppSEM appSEM) {
+		if (appSEM.estaEnZonaEstacionamiento()) {
+			appSEM.getModo().inicioEstacionamiento(appSEM);
+		}
+			
+	}
 
 }
--- test/ar/edu/unq/poo2/tpfinal/NoVigenteTest.java
+++ test/ar/edu/unq/poo2/tpfinal/NoVigenteTest.java
@@ -1,6 +1,11 @@
 package ar.edu.unq.poo2.tpfinal;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,18 +15,72 @@ class NoVigenteTest {
 
 	NoVigente estadoNoVigente;
 	AppSEM mockApp;
+	SEM mockSEM;
 
 	@BeforeEach
 	void setUp() throws Exception {
 
 		estadoNoVigente = new NoVigente();
 		mockApp = Mockito.mock(AppSEM.class);
+		mockSEM = Mockito.mock(SEM.class);
 	}
 
 	@Test
-	void test1_CuandoEsEstadoNoVigenteIniciarInteractuaConLaApp() {
+	void test1_CuandoEsEstadoNoVigenteIniciarInteractuaConLaAppConSaldoPositivo() {
+
+		when(mockApp.tieneSaldoMinimo()).thenReturn(true);
+		when(mockApp.getSistema()).thenReturn(mockSEM);
+
+		estadoNoVigente.iniciar(mockApp, "AAA-111");
+
+		verify(mockSEM, times(1)).estacionamientoIniciado(mockApp, "AAA-111");
+		verify(mockApp, times(1)).setEstado(argThat(estacionado -> estacionado instanceof Vigente));
+		verify(mockApp, times(0)).notificarSaldoInsuficiente();
+
+	}
+
+	@Test
+	void test2_CuandoEsEstadoNoVigenteIniciarInteractuaConLaAppConSaldoInsuficiente() {
+
+		when(mockApp.tieneSaldoMinimo()).thenReturn(false);
+		when(mockApp.getSistema()).thenReturn(mockSEM);
+
+		estadoNoVigente.iniciar(mockApp, "AAA-111");
+
+		verify(mockSEM, times(0)).estacionamientoIniciado(mockApp, "AAA-111");
+		verify(mockApp, times(0)).setEstado(argThat(estacionado -> estacionado instanceof Vigente));
+		verify(mockApp, times(1)).notificarSaldoInsuficiente();
+
+	}
+
+	@Test
+	void test3_CuandoEsEstadoNoVigenteFinalizarNoInteractuaConApp() {
+		estadoNoVigente.finalizar(mockApp);
+
+		verifyNoInteractions(mockApp);
+	}
+
+	@Test
+	void test4_CuandoEsEstadoNoVigenteInicioNotificadoInteractuaConApp() {
 		
+		Manual mockManual = Mockito.mock(Manual.class);
 		
+		when(mockApp.estaEnZonaEstacionamiento()).thenReturn(true);
+		when(mockApp.getModo()).thenReturn(mockManual);
+	
+		estadoNoVigente.inicioNotificado(mockApp);
+		
+		verify(mockManual,times(1)).inicioEstacionamiento(mockApp);
+		
+	
+	}
+	
+	@Test
+	void test5_CuandoEsEstadoVigenteFinNotificadoNoInteractuaConApp() {
+		estadoNoVigente.finNotificado(mockApp);
+		
+		verifyNoInteractions(mockApp);
+				
 	}
 
 }
