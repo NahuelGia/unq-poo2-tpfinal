878f195ea834a6a1a1b66973876f89478b6990a2
Augusto Piana <augustopiana3@gmail.com>
2024-06-12 00:19:11 -0300

    agrego comentarios @Override

--- src/ar/edu/unq/poo2/tpfinal/Vigente.java
+++ src/ar/edu/unq/poo2/tpfinal/Vigente.java
@@ -2,11 +2,13 @@ package ar.edu.unq.poo2.tpfinal;
 
 public class Vigente extends EstadoEstacionamiento {
 
+	@Override
 	public void finalizar(AppSEM appSEM) {
 		appSEM.getSistema().finalizarEstacionamiento(appSEM);
 		appSEM.setEstado(new NoVigente());
 	}
 
+	@Override
 	public void finNotificado(AppSEM appSEM) {
 		
 		if (appSEM.estaEnZonaEstacionamiento()) {
