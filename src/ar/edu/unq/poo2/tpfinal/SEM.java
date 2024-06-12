4f75282632ae130968888cfab98ac04124e00fed
Ignacio Diaz <xxnachoxxt.m@gmail.com>
2024-06-12 17:32:21 -0300

    prueba

--- src/ar/edu/unq/poo2/tpfinal/SEM.java
+++ src/ar/edu/unq/poo2/tpfinal/SEM.java
@@ -2,4 +2,14 @@ package ar.edu.unq.poo2.tpfinal;
 
 public class SEM {
 
+	public Boolean consultarVigencia(String patente) {
+		
+		return true;//esta hardcodeado el valor true
+	}
+
+	public void registrarInfraccion(Infraccion infraccion) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
