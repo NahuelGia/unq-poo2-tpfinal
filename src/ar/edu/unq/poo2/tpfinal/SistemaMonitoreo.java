db409c52984a28d27e1739aaa55c7c1645a46340
Nahuel Giamminelli <nahuelgiamminelli@gmail.com>
2024-06-12 22:32:38 -0300

    SistemaMonitoreo

---
+++ src/ar/edu/unq/poo2/tpfinal/SistemaMonitoreo.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.poo2.tpfinal;
+
+public interface SistemaMonitoreo {
+
+	public void updateInicioEstacionamiento(SEM sem, EstacionadoAPP nuevoEstacionado);
+
+	public void updateFinEstacionamiento(SEM sem, EstacionadoAPP estacionado);
+
+}
